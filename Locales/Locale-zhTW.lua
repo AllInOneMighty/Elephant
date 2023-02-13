c756c84a9575113e3438b4bff62c190605081680
AllInOneMighty <mighty@allinonemighty.com>
2023-02-13 02:28:00 -0800

    Fixing weird... space, apparently.

--- Locales/Locale-zhTW.lua
+++ Locales/Locale-zhTW.lua
@@ -1,4 +1,4 @@
-﻿local L = LibStub("AceLocale-3.0"):NewLocale("Elephant", "zhTW")
+local L = LibStub("AceLocale-3.0"):NewLocale("Elephant", "zhTW")
 if not L then return end
 
 --[[ Common messages ]]
